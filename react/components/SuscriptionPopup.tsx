fe98f800b4fabf9421e92ae01252e6dfe58c2ac8
camilo-velandia <camilo.velandia@itglobers.com>
2022-07-21 16:06:52 -0500

    add modal and close validation

--- react/components/SuscriptionPopup.tsx
+++ react/components/SuscriptionPopup.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { Modal } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
 
-const SuscriptionPopup = () => {
+
+type Props = {
+  image: string
+  text: string
+}
+
+const SuscriptionPopup = ({image, text}:Props) => {
+
+  const CSS_HANDLES = ['closeBtn','mainContainer','infoText','formContainer']
+  const handles = useCssHandles(CSS_HANDLES)
+
+
+  const [modal, setModal] = useState(false)
+
+  const isClosed = localStorage.getItem('hiddeModal')
+
+  useEffect(() => {
+    if (isClosed === 'true') {
+      setModal(false)
+    } else {
+      setTimeout(() => { setModal(true) }, 2000);
+    }
+  }, [])
+
+  const handleClose = () => {
+    setModal(false)
+    localStorage.setItem('hiddeModal', 'true')
+  }
+
+  console.log('text',text)
+
   return (
     <div>
-      SuscriptionPopup
+      <Modal centered isOpen={modal} onClose={handleClose}>
+
+        <div
+          className={handles.main__container}
+          style={{
+            backgroundImage: `url(${image})`
+          }}>
+            <h3 className={handles.infoText}>
+              {text}
+            </h3>
+            <div>form</div>
+        </div>
+      </Modal>
     </div>
-  );
+  )
+
+
 };
+
+SuscriptionPopup.schema = {
+  title: "Suscription Popup",
+  type: "object",
+  properties: {
+    image: {
+      title: "Upload modal image",
+      type: 'string',
+      widget: {
+        "ui:widget": "image-uploader"
+      }
+    },
+
+    text: {
+      title: "informative text",
+      type: 'string'
+    }
+  }
+
+}
 
 export default SuscriptionPopup;
